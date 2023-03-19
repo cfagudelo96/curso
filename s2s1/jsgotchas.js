1ab3f1713b061b45c374bc24e0a8f7e46cb506a7
Carlos Agudelo <cf.agudelo96@gmail.com>
2023-03-19 18:58:18 -0500

    first commit

---
+++ s2s1/jsgotchas.js
@@ -0,0 +1,1 @@
+// https://www.codeproject.com/Articles/182416/A-Collection-of-JavaScript-Gotchas#functions-operators
---
+++ s2s1/objetos.ts
@@ -0,0 +1,6 @@
+/*
+El tipo object es uno de los mas comunes en JS.
+Hace referencia a un valor con propiedades
+Sintaxis:
+{ propiedad1: tipoPropiedad1, propiedad2: tipoPropiedad2, propiedadOpcional?: tipoPropiedadOpcional }
+*/
