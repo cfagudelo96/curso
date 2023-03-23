ead7f74360d47e4b646969597991d92ba34c949e
Carlos Agudelo <cf.agudelo96@gmail.com>
2023-03-23 18:04:13 -0500

    s2s3

---
+++ s2s3/ejObjetos.ts
@@ -0,0 +1,26 @@
+/**
+ * Quiero desarrollar una aplicación para hacer seguimientos a estudiantes de una sección.
+ * Quiero poder saber sus nombres, y las notas que han obtenido para cada tarea/actividad.
+ * Cada nota tiene un peso, expresado como un porcentaje de la nota total.
+ * 
+ * Por ejemplo, Andrés Lopez, ha obtenido 3 notas:
+ *  4 (10% de la nota total), 4.5 (5% de la nota total), 3.5 (15% de la nota total)
+ */
+
+/**
+ * Viva Air decidió hacer reembolso de tiquetes comprados.
+ * La información relevante para el reembolso de un tiquete es:
+ * - Fecha de compra
+ * - Valor de compra
+ * - Itinerario
+ * El itinerario de un tiquete esta determinado por el lugar de origen, el lugar destino,
+ *   la fecha de salida, con hora de salida, y el número de avión
+ */
+
+/**
+ * Contador de primeros nombres
+ */
+
+/**
+ * Contador de cumpleaños repetidos
+ */
